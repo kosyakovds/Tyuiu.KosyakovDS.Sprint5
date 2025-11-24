5636ef71fd14d9e16631fec556e14cd42c9b314e
kosyakovds <kosyakov27@mail.ru>
2025-11-24 19:38:49 +0500

    Task0 V24

--- Tyuiu.KosyakovDS.Sprint5.Task0.V24.Lib/DataService.cs
+++ Tyuiu.KosyakovDS.Sprint5.Task0.V24.Lib/DataService.cs
@@ -9,8 +9,7 @@ namespace Tyuiu.KosyakovDS.Sprint5.Task0.V24.Lib
         {
             string path = $@"{Directory.GetCurrentDirectory()}\OutPutFileTask0.txt";
 
-            double y = (Math.Pow(x, 3) - 8) / (2 * Math.Pow(x, 2));
-            double y = Math.Round(y, 3);
+            double y = Math.Round((Math.Pow(x, 3) - 8) / (2 * Math.Pow(x, 2)), 3);
 
             File.WriteAllText(path, Convert.ToString(y));
             return path;
