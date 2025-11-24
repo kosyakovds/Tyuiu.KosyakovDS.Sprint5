4f3c9da997ed80e3fa7092f7e1cdf76142d6bb52
kosyakovds <kosyakov27@mail.ru>
2025-11-24 21:31:50 +0500

    Task1 V3

--- Tyuiu.KosyakovDS.Sprint5.Task1.V3.Lib/DataService.cs
+++ Tyuiu.KosyakovDS.Sprint5.Task1.V3.Lib/DataService.cs
@@ -8,7 +8,7 @@ namespace Tyuiu.KosyakovDS.Sprint5.Task1.V3.Lib
     {
         public string SaveToFileTextData(int startValue, int stopValue)
         {
-            string path = $@"{Directory.GetCurrentDirectory()}\OutPutFileTask1.txt";
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "OutPutFileTask1.txt");
 
             FileInfo fileInfo = new FileInfo(path);
             bool fileExists = fileInfo.Exists;
