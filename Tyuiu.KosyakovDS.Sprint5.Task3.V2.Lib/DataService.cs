343e39f059e7c872636c3d3887be84fc27b54dba
kosyakovds <kosyakov27@mail.ru>
2025-11-25 20:43:24 +0500

    Task3 V2

---
+++ Tyuiu.KosyakovDS.Sprint5.Task3.V2.Lib/DataService.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.KosyakovDS.Sprint5.Task3.V2.Lib
+{
+    public class DataService : ISprint5Task3V2
+    {
+        public string SaveToFileTextData(int x)
+        {
+            string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask3.bin");
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+
+            double y = Math.Round(Math.Exp(x) / x, 3);
+
+            using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.OpenOrCreate)))
+            {
+                writer.Write(BitConverter.GetBytes(y));
+            }
+
+            return path;
+        }
+    }
+}
---
+++ Tyuiu.KosyakovDS.Sprint5.Task3.V2.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿namespace Tyuiu.KosyakovDS.Sprint5.Task3.V2.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedExistsFile()
+        {
+            string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask3.bin");
+            FileInfo fileInfo = new FileInfo(path);
+
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
